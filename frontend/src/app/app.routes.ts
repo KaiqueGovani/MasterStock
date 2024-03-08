7843ec46c8016ca32cadc157c5a97e5ce7bd19b7
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-03-07 21:52:08 -0300

    Definidas as rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './paginas/login/login.component';
+import { DashboardComponent } from './paginas/dashboard/dashboard.component';
+import { ListaDeComprasComponent } from './paginas/lista-de-compras/lista-de-compras.component';
+import { ProdutosComponent } from './paginas/produtos/produtos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'lista-de-compras', component: ListaDeComprasComponent },
+  { path: 'Produtos', component: ProdutosComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
