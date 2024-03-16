7a44f3a97ab793ceccc6d0cb2eefef680c03e404
kaique.govani <kaique.govani@hotmail.com>
2024-03-16 15:00:05 -0300

    Fixing database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, DatabaseModule],
+  imports: [ProductsModule, ConfigModule.forRoot({isGlobal: true}), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import DatabaseConfig from './database.config';
 
 @Module({
-    imports: [MongooseModule.forRoot(`mongodb://${process.env.MONGO_ROOT_USER}:${process.env.MONGO_ROOT_PASSWORD}@mongo:27017/MasterStock`)],
+    imports: [
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    uri: DatabaseConfig.getMongoConfig(configService),
+                };
+            },
+            inject: [ConfigService],
+        }),
+    ],
     exports: [MongooseModule],
 })
 export class DatabaseModule {}
