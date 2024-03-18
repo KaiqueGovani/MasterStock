28c8a2c9265a7f7e4ccd2a86be5d0bc5b93471ff
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2024-03-18 04:07:51 -0300

    [Backend] - Iniciando Auth e Users Resources / Watch files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './common/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './api/auth/auth.module';
+import { ProductsModule } from './api/products/products.module';
+import { UsersModule } from './api/users/users.module';
 
 @Module({
-  imports: [ProductsModule, ConfigModule.forRoot({isGlobal: true}), DatabaseModule],
+  imports: [
+    ProductsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentFactory } from './common/docs/document.factory';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configure Swagger Document
-  const config = new DocumentBuilder()
-  .setTitle('MasterStock API')
-  .setDescription('The MasterStock API reference documentation')
-  .setVersion('1.0')
-  .addTag('products')
-  .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // Instantiate Swagger app
+  DocumentFactory.create(app);
 
   await app.listen(3000);
 }
