3c982a1ad7784f9b758c147c1f662983ccb17727
kaique.govani <kaique.govani@hotmail.com>
2024-04-02 20:08:36 -0300

    [Backend] - Mockando valores get /products

--- backend/src/api/auth/auth.controller.ts
+++ backend/src/api/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- backend/src/api/products/products.service.ts
+++ backend/src/api/products/products.service.ts
@@ -9,7 +9,56 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return [
+      {
+        id: 1,
+        nome: 'Coca-cola',
+        quantidade: 2,
+        desejado: 5,
+        preco: 7.99,
+        descricao: '2 litros',
+        dataCompra: new Date(),
+        dataValidade: new Date(),
+        urlImagem:
+          'https://loja.supermerclick.com.br/image/cache/catalog/produtos-integracao/003851-omie___coca-ls-1litro__conv-1000x1000.jpg',
+      },
+      {
+        id: 2,
+        nome: 'Coca-cola',
+        quantidade: 3,
+        desejado: 6,
+        preco: 7.99,
+        descricao: '2 litros',
+        dataCompra: new Date(),
+        dataValidade: new Date(),
+        urlImagem:
+          'https://loja.supermerclick.com.br/image/cache/catalog/produtos-integracao/003851-omie___coca-ls-1litro__conv-1000x1000.jpg',
+      },
+      {
+        id: 3,
+        nome: 'Coca-cola',
+        quantidade: 1,
+        desejado: 4,
+        preco: 7.99,
+        descricao: '2 litros',
+        dataCompra: new Date(),
+        dataValidade: new Date(),
+        urlImagem:
+          'https://loja.supermerclick.com.br/image/cache/catalog/produtos-integracao/003851-omie___coca-ls-1litro__conv-1000x1000.jpg',
+      },
+      {
+        id: 4,
+        nome: 'Coca-cola',
+        quantidade: 6,
+        desejado: 9,
+        preco: 7.99,
+        descricao: '2 litros',
+        dataCompra: new Date(),
+        dataValidade: new Date(),
+        urlImagem:
+          'https://loja.supermerclick.com.br/image/cache/catalog/produtos-integracao/003851-omie___coca-ls-1litro__conv-1000x1000.jpg',
+      },
+    ];
   }
 
   findOne(id: number) {
