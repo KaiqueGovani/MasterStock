362abae04a473b134edb62ea28187844808f65f9
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2024-04-17 20:38:31 -0300

    [ DevOps ] - Migrando Mongo para Cloud

--- backend/src/common/database/database.config.ts
+++ backend/src/common/database/database.config.ts
@@ -1,9 +1,8 @@
 import { ConfigService } from '@nestjs/config';
 
 export default class DatabaseConfig {
-  static getMongoConfig(configService: ConfigService) {
-    const user = configService.get<string>('MONGO_ROOT_USER');
-    const password = configService.get<string>('MONGO_ROOT_PASSWORD');
-    return `mongodb://${user}:${password}@mongo/MasterStock?authSource=admin`;
+  static getMongoConfig(configService: ConfigService): string {
+    const url = configService.get<string>('MONGO_CLOUD_CLUSTER_DB_URL');
+    return url;
   }
 }
