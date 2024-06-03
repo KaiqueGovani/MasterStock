57b78d431e5df71e97c18d24cb1dc0e4d040764f
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2024-06-02 23:52:30 -0300

    [ Backend ] - Mock qrcode info

--- backend/src/api/qrcode/qrcode.controller.ts
+++ backend/src/api/qrcode/qrcode.controller.ts
@@ -19,6 +19,58 @@ export class QrcodeController {
   @Post('/')
   async getQrCode(@Body() getQrCodeDto: GetQrCodeDto) {
     try {
+      // Mocking for testing purposes
+      const data = {
+        data_hora: '2024-03-20 07:10:54',
+        nome_razao_social: 'PERALTA DISTRIBUIDORA DE ALIMENTOS LTDA',
+        numero_cfe: '049723',
+        produtos: [
+          {
+            codigo: '00000000037563',
+            descricao: 'CAQUI CHOCOLATE',
+            imagem: null,
+            qtd_comercial: '0,4400',
+            valor_bruto: '6,55',
+            valor_unitario: '14,900',
+          },
+          {
+            codigo: '00000000017947',
+            descricao: 'PERA PORTUGUESA',
+            imagem: null,
+            qtd_comercial: '0,2750',
+            valor_bruto: '5,19',
+            valor_unitario: '18,900',
+          },
+          {
+            codigo: '00000000015127',
+            descricao: 'BANANA NANICA',
+            imagem: null,
+            qtd_comercial: '0,4050',
+            valor_bruto: '2,22',
+            valor_unitario: '5,490',
+          },
+          {
+            codigo: '00000000019392',
+            descricao: 'PAO FRANCES KG',
+            imagem: null,
+            qtd_comercial: '0,1800',
+            valor_bruto: '2,87',
+            valor_unitario: '15,980',
+          },
+          {
+            codigo: '00000000049115',
+            descricao: 'AMEIXA FRESCA I',
+            imagem: null,
+            qtd_comercial: '0,3900',
+            valor_bruto: '5,06',
+            valor_unitario: '12,980',
+          },
+        ],
+        valor_completo: '21,89',
+      };
+
+      return { data, message: 'Url do QrCode lido com sucesso! Produtos obtidos via web-scraping.' };
+
       const regex = /\b\d{44}\b/;
       const match = getQrCodeDto.read_content.match(regex);
       const extractedNumber = match ? match[0] : null;
@@ -30,7 +82,7 @@ export class QrcodeController {
       const response = await fetch('http://bot:5000/consultar-cfe/' + extractedNumber);
       Logger.debug('Finished fetch!');
       Logger.debug('response', response);
-      const data = await response.json();
+      // const data = await response.json();
       return { data, message: 'Url do QrCode lido com sucesso! Produtos obtidos via web-scraping.' };
     } catch (error) {
       console.error('error', error);
